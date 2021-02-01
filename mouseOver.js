675a39200b644a6dbb9a021369c5de3957737fdd
abdalkadermorgan <77734227+abdalkadermorgan@users.noreply.github.com>
2021-02-01 18:55:47 +0300

    Delete mouseOver.js

--- mouseOver.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("test").onmouseover = function () { mouseOver(); };
-document.getElementById("test").onmouseout = function () { mouseOut(); };
-function mouseOver() {
-    document.getElementById("item").style.display = "block";
-}
-function mouseOut() {
-    document.getElementById("item").style.display = "none";
-}
